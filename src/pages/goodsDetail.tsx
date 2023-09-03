54cf8b2d3f32dccc8444a2b875a9d0b0e1afc112
rintaro1029 <tomitaka1029@gmail.com>
2023-09-03 17:31:16 +0900

    #

---
+++ src/pages/goodsDetail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const goodsDetail = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-3xl font-bold">Coming Soon</p>
+    </div>
+  );
+};
+
+export default goodsDetail;
